7bd010b0e277b057b1b3267b3f2a3b6d57457473
Johan Berg <johan.berg@abitsure.com>
2019-12-14 16:38:44 +0100

    18 updated test

--- test/routes/02_user.spec.ts
+++ test/routes/02_user.spec.ts
@@ -25,140 +25,156 @@ describe('userRoute', async () => {
 
   let token
 
-  before((done) => {
-    expect(UserModel.modelName).to.be.equal('User')
+    before((done) => {
+      expect(UserModel.modelName).to.be.equal('User')
 
-    UserModel.collection.drop(async (error, result) => {
-      try {
-        const response = await result
-        const newUser = new UserModel(user)
-        if (response) {
-          newUser.password = bcrypt.hashSync(newUser.password, 10)
-        }
-
-        OrderAPILogger.logger.info(
-          'calling save to create a default user for loging'
-        )
-
-        newUser.save(async (err, userCreated) => {
-          try {
-            user._id = await userCreated._id
-            done()
-          } catch (error) {
-            throw new Error(err.message)
+      UserModel.collection.drop(async (error, result) => {
+        try {
+          const response = await result
+          const newUser = new UserModel(user)
+          if (response) {
+            newUser.password = bcrypt.hashSync(newUser.password, 10)
           }
-        })
-      } catch (err) {
-        throw new Error(error.message)
-      }
+
+          OrderAPILogger.logger.info(
+            'calling save to create a default user for loging'
+          )
+
+          newUser.save(async (err, userCreated) => {
+            try {
+              user._id = await userCreated._id
+              done()
+            } catch (error) {
+              throw new Error(err.message)
+            }
+          })
+        } catch (err) {
+          throw new Error(error.message)
+        }
+      })
     })
-  })
 
   it('should be able to login', async () => {
+      try {
+        const res = await chai
+          .request(app)
+          .get(`/users/login?username=${user.username}&password=${user.password}`)
+        expect(res.status).to.be.equal(200)
+        token = await res.body.token
+      } catch (err) {
+        throw new Error(err.message)
+      }
+    })  
+
+  it('should respond with HTTP 404 status because there is no user', async () => {
     try {
       const res = await chai
+      .request(app)
+      .get(`/users/NO_USER`)
+      .set('Authorization', `Bearer ${token}`)
+      expect(res.status).to.be.equal(404)
+    }
+    catch (err) {
+      throw new Error(err.message)
+    }
+  })
+  it('should create a new user and retrieve it back', async () => {
+    try {
+      user.email = 'unique_email@email.com'
+      const res = await chai
         .request(app)
-        .get(`/users/login?username=${user.username}&password=${user.password}`)
-      expect(res.status).to.be.equal(200)
-      token = await res.body.token
+        .post('/users')
+        .set('Authorization', `Bearer ${token}`)
+        .send(user)
+        expect(res.status).to.be.equal(201)
+        expect(res.body.username).to.be.equal(user.username)
     } catch (err) {
       throw new Error(err.message)
     }
   })
-
-  it('should respond with HTTP 404 status because there is no user', () => {
-    return chai
-      .request(app)
-      .get(`/users/NO_USER`)
-      .set('Authorization', `Bearer ${token}`)
-      .then(res => {
-        expect(res.status).to.be.equal(404)
-      })
-  })
-  it('should create a new user and retrieve it back', () => {
-    user.email = 'unique_email@email.com'
-    return chai
-      .request(app)
-      .post('/users')
-      .set('Authorization', `Bearer ${token}`)
-      .send(user)
-      .then(res => {
-        expect(res.status).to.be.equal(201)
-        expect(res.body.username).to.be.equal(user.username)
-      })
-  })
-  it('should return the user created on the step before', () => {
-    return chai
-      .request(app)
-      .get(`/users/${user.username}`)
-      .set('Authorization', `Bearer ${token}`)
-      .then(res => {
+  it('should return the user created on the step before', async () => {
+      try {
+        const res = await chai
+          .request(app)
+          .get(`/users/${user.username}`)
+          .set('Authorization', `Bearer ${token}`)
         expect(res.status).to.be.equal(200)
         expect(res.body.username).to.be.equal(user.username)
-      })
+      } catch (err) {
+        throw new Error(err.message)
+      } 
   })
-  it('should updated the user John', () => {
-    user.username = 'John_Updated'
-    user.firstName = 'John Updated'
-    user.lastName = 'Doe Updated'
-    user.email = 'John@myemail_updated.com'
-    user.password = 'password Updated'
-    user.phone = '3333333'
-    user.userStatus = 12
+  it('should updated the user John', async () => {
+    try {
+      user.username = 'John_Updated'
+      user.firstName = 'John Updated'
+      user.lastName = 'Doe Updated'
+      user.email = 'John@myemail_updated.com'
+      user.password = 'password Updated'
+      user.phone = '3333333'
+      user.userStatus = 12
 
-    return chai
-      .request(app)
-      .patch(`/users/John`)
-      .set('Authorization', `Bearer ${token}`)
-      .send(user)
-      .then(res => {
-        expect(res.status).to.be.equal(204)
-      })
+      const res = await chai
+        .request(app)
+        .patch(`/users/John`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(user)
+      expect(res.status).to.be.equal(204)
+    } catch (err) {
+      throw new Error(err.message)
+    } 
   })
-  it('should return the user updated on the step before', () => {
-    return chai
-      .request(app)
-      .get(`/users/${user.username}`)
-      .set('Authorization', `Bearer ${token}`)
-      .then(res => {
-        expect(res.status).to.be.equal(200)
-        expect(res.body.username).to.be.equal(user.username)
-        expect(res.body.firstName).to.be.equal(user.firstName)
-        expect(res.body.lastName).to.be.equal(user.lastName)
-        expect(res.body.email).to.be.equal(user.email)
-        expect(res.body.password).to.be.equal(user.password)
-        expect(res.body.phone).to.be.equal(user.phone)
-        expect(res.body.userStatus).to.be.equal(user.userStatus)
-      })
+  it('should return the user updated on the step before', async () => {
+    try {
+      const res = await chai
+        .request(app)
+        .get(`/users/${user.username}`)
+        .set('Authorization', `Bearer ${token}`)
+      expect(res.status).to.be.equal(200)
+      expect(res.body.username).to.be.equal(user.username)
+      expect(res.body.firstName).to.be.equal(user.firstName)
+      expect(res.body.lastName).to.be.equal(user.lastName)
+      expect(res.body.email).to.be.equal(user.email)
+      expect(res.body.password).to.be.equal(user.password)
+      expect(res.body.phone).to.be.equal(user.phone)
+      expect(res.body.userStatus).to.be.equal(user.userStatus)
+    } catch (err) {
+      throw new Error(err.message)
+    } 
   })
-  it('should return 404 because the user does not exist', () => {
-    user.firstName = 'Mary Jane'
-
-    return chai
-      .request(app)
-      .patch(`/users/Mary`)
-      .set('Authorization', `Bearer ${token}`)
-      .send(user)
-      .then(res => {
+  it('should return 404 because the user does not exist', async () => {
+    try {
+      user.firstName = 'Mary Jane'
+      const res = await chai
+        .request(app)
+        .patch(`/users/Mary`)
+        .set('Authorization', `Bearer ${token}`)
+        .send(user)
+      expect(res.status).to.be.equal(404)
+    } catch (err) {
+      throw new Error(err.message)
+    } 
+  })
+  it('should remove an existent user', async () => {
+    try {
+      const res = await chai
+        .request(app)
+        .del(`/users/${user.username}`)
+        .set('Authorization', `Bearer ${token}`)
+      expect(res.status).to.be.equal(204)
+    } catch (err) {
+      throw new Error(err.message)
+    } 
+  })
+  it('should return 404 when it is trying to remove an user because the user does not exist', async () => {
+      try {
+        const res = await chai
+          .request(app)
+          .del(`/users/Mary`)
+          .set('Authorization', `Bearer ${token}`)
         expect(res.status).to.be.equal(404)
-      })
-  })
-  it('should remove an existent user', () => {
-    return chai
-      .request(app)
-      .del(`/users/${user.username}`)
-      .set('Authorization', `Bearer ${token}`)
-      .then(res => {
-        expect(res.status).to.be.equal(204)
-      })
-  })
-  it('should return 404 when it is trying to remove an user because the user does not exist', () => {
-    return chai
-      .request(app)
-      .del(`/users/Mary`)
-      .set('Authorization', `Bearer ${token}`)
-      .then(res => {
-        expect(res.status).to.be.equal(404)
-      })
+      } catch (err) {
+        throw new Error(err.message)
+      } 
   })
 })
