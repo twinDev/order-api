25a609c21044ade4e2cde379ad0d7a43e5e75706
Johan Berg <johan.berg@abitsure.com>
2019-12-15 00:15:13 +0100

    33 updated app

--- src/app.ts
+++ src/app.ts
@@ -3,10 +3,10 @@ import * as express from 'express'
 import * as expressWinston from 'express-winston'
 import * as mongoose from 'mongoose'
 import * as winston from 'winston'
-import { APIRoute } from '../src/routes/api'
-import { OrderRoute } from '../src/routes/order'
-import { UserRoute } from '../src/routes/user'
-import * as errorHandler from '../src/utility/errorHandler'
+import { APIRoute } from './routes/api'
+import { OrderRoute } from './routes/order'
+import { UserRoute } from './routes/user'
+import * as errorHandler from './utility/errorHandler'
 
 class App {
   public app: express.Application
